316e2b57a5624bbfd44cd7796a67320f3ad281bd
Gerard Sala <gerard.sala@outlook.com>
2017-01-28 19:08:01 +0100

    Change API declaration

--- app/src/main/java/com/saladevs/changelogclone/network/PlayStoreService.java
+++ app/src/main/java/com/saladevs/changelogclone/network/PlayStoreService.java
@@ -1,11 +1,11 @@
 package com.saladevs.changelogclone.network;
 
 import retrofit2.http.GET;
-import retrofit2.http.Query;
+import retrofit2.http.Path;
 import rx.Observable;
 
 public interface PlayStoreService {
 
-    @GET("changelogs/")
-    Observable<PackageChangelog> getChangelog(@Query("package") String packageName);
+    @GET("packages/{packageName}/scrape")
+    Observable<PackageChangelog> getChangelog(@Path("packageName") String packageName);
 }
